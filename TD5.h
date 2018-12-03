3cbfca6031dff88fbc1f3525be594824aa5d8bec
gl2206 <45152976+gl2206@users.noreply.github.com>
2018-12-03 10:06:54 +0100

    Add files via upload

---
+++ TD5.h
@@ -0,0 +1,15 @@
+#ifndef TD5_H_INCLUDED
+#define TD5_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct
+{
+    int mat[10][10];
+    int size;
+} Matrice;
+
+#endif // TD5_H_INCLUDED
